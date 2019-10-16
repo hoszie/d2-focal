6bf146e6ccd1458b9b60a3e72255b7ebb4cd245b
Nick <nhoszko@gmail.com>
2019-10-16 01:03:04 +0000

    renamed directories

---
+++ debugging/reverse.js
@@ -0,0 +1,11 @@
+var input = process.argv[2];
+function reversed(input) {
+  return input.split('').reverse().join('');
+}
+if (input) {
+  console.log(reversed(input));
+}
+
+// making some change for pushing
+
+
